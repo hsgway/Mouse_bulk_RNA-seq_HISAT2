577da006fb39b8d064e64ca55fcf9064a3fa56f8
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-10 00:02:37 -0500

    Update 0_md5check_BPF.sh

--- 0_md5check_BPF.sh
+++ 0_md5check_BPF.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # file and directory
-md5file="/Volumes/TOSHIBA/20220729_TimeCourse_Colon/0_FASTQ/FC_07570/Unaligned_12_PF_mm1/FC_07570_Unaligned_12_PF_mm1.md5"
-md5file_new="/Volumes/TOSHIBA/20220729_TimeCourse_Colon/0_FASTQ/FC_07570/Unaligned_12_PF_mm1/FC_07570_Unaligned_12_PF_mm1_new.md5"
-dir="/Volumes/TOSHIBA/20220729_TimeCourse_Colon/0_FASTQ"
+md5file="xxx.md5"
+md5file_new="xxx_new.md5"
+dir="Path_to_FASTQ_directory"
 
 # modify md5 file
-cat $md5file | sed -e 's/\/data\/users\/yhasegawa\///g' > $md5file_new
+cat $md5file | sed -e 's/\/data\/users\/xxx\///g' > $md5file_new
 
 # go to the folder containing your files
 cd $dir
